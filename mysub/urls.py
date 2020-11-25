408450f444b21d57644b78ac2b16e37aad9addf7
Amos Vryhof <amos@vryhofresearch.com>
2020-11-25 16:20:55 -0500

    Remove unused import

--- mysub/urls.py
+++ mysub/urls.py
@@ -18,8 +18,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from api_auth.views import callback_view
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("o/", include("oauth2_provider.urls", namespace="oauth2_provider")),
