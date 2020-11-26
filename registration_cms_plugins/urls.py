108de8f023dbad975a5143a7e696541ff2c9d320
Amos Vryhof <amos@vryhofresearch.com>
2020-11-26 10:24:53 -0500

    Make captcha registration the default

--- registration_cms_plugins/urls.py
+++ registration_cms_plugins/urls.py
@@ -4,5 +4,5 @@ from registration_cms_plugins.views import log_out_view, CaptchaRegistrationView
 
 urlpatterns = [
     path("log-out/", log_out_view, name="log-out-view"),
-    path("captcha-registration/", CaptchaRegistrationView.as_view(), name="captcha-registration")
+    path("register/", CaptchaRegistrationView.as_view(), name="captcha-registration")
 ]
